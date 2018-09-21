b8ef181aa9f477b6e41a440beea254d2fa1bd3e2
Tommy Do <tommy.td89@gmail.com>
2018-09-20 21:18:44 -0400

    added Projects page

--- server.js
+++ server.js
@@ -55,6 +55,12 @@ app.get('/bad', (req, res) => {
 	})
 })
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle:'Project',
+		message: 'Hi, these are my projects'
+	});
+});
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
